dce0a89314bd1704ae5fec4a9cb4782ecc2ca382
RASTIER BENOIT <benoit.rastier@gmail.com>
2017-06-03 11:42:21 +0200

    Suppression des fichier non necessaire.

--- osTicket/upload/include/plugins/extract.php
+++
@@ -1,9 +0,0 @@
-<?php
-try {
-$phar = new Phar('fr.phar');
-$phar->extractTo('./',null,true); // extract all files
-} catch (Exception $e) {
-echo "there was an error<br>";
-print_r($e);
-}
-?>
--- osTicket/upload/scp/contrat.php
+++
@@ -1,6 +0,0 @@
-<?php
-require('staff.inc.php');
-
-require_once(STAFFINC_DIR.'header.inc.php');
-require_once(STAFFINC_DIR.'contrats.inc.php');
-require_once(STAFFINC_DIR.'footer.inc.php');
